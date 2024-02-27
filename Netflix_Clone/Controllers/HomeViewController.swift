4fb03ebdd94747d9de7c51d0f9f0b62e1de0c1d6
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2024-02-27 15:07:17 -0800

    added-basic-table-view

--- Netflix_Clone/Controllers/HomeViewController.swift
+++ Netflix_Clone/Controllers/HomeViewController.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet var homeFeedTable: UITableView!
+//    private let homeFeedTable: UITableView = {
+//        let table = UITableView()
+//        table.register(UITableView.self, forCellReuseIdentifier: "cell")
+//        return table
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
-
+        view.addSubview(homeFeedTable)
         // Do any additional setup after loading the view.
+
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews();
+        homeFeedTable.frame = view.bounds
     }
     
 
@@ -28,4 +41,18 @@ class HomeViewController: UIViewController {
     }
     */
 
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "HELLO WORLD"
+        return cell
+    }
+    
+    
 }
