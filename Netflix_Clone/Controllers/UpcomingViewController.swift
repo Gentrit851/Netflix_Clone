4264805bbf0b961b4b58079f6ffd0b7009304007
Gentrit Kryeziu <gentikryeziu851@gmail.com>
2024-02-28 23:39:37 +0100

    get the upcoming movies using API

--- Netflix_Clone/Controllers/UpcomingViewController.swift
+++ Netflix_Clone/Controllers/UpcomingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     
+    private var titles: [Title] = [Title]()
+    
     private let upcomingTable: UITableView = {
        let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -27,6 +29,8 @@ class UpcomingViewController: UIViewController {
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
         upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
     
     override func viewDidLayoutSubviews() {
@@ -34,17 +38,32 @@ class UpcomingViewController: UIViewController {
         upcomingTable.frame = view.bounds
     }
     
+    private func fetchUpcoming(){
+        APICaller.shared.getUpcomingMovies{ [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                    
+                  }
+            case .failure (let error):
+                print(error.localizedDescription)
+               }
+           }
+        }
 }
+
 
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titles[indexPath.row].original_title ?? titles[indexPath.row].original_title ?? "Unknown"
         return cell
     }
     
