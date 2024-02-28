9b4caf3e9d577c31c620faa73a23aceffca7f8f9
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2024-02-28 09:52:21 -0800

    added-fetch-on-front

--- Netflix_Clone/Managers/APICaller.swift
+++ Netflix_Clone/Managers/APICaller.swift
@@ -12,10 +12,14 @@ struct Constants{
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error{
+    case failedToGetData
+}
+
 class APICaller{
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> Void){
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void){
         
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
         
@@ -26,9 +30,10 @@ class APICaller{
             
             do{
                 let results = try JSONDecoder().decode(TredingMoviesResponse.self, from: data)
-                print(results)
+                completion(.success(results.results))
+                
             }catch{
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
             
         }
