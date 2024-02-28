ea4885b1002572a366f8c87df081b233f97ce403
Gentrit Kryeziu <gentikryeziu851@gmail.com>
2024-02-29 00:28:54 +0100

    Get the movies data and add it to the Upcoming View Controller

---
+++ Netflix_Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix_Clone
+//
+//  Created by Gentrit on 29.2.24.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}
