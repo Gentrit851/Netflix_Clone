11b2538ba1c3f794204c654fe9625775a2eeb402
Gentrit Kryeziu <gentikryeziu851@gmail.com>
2024-02-29 04:00:52 +0100

    add search functionality and fix the navigation controller part
    This part of the code add the functionality of the Search bar in the Search View Controller by using the TMDB API and fixes the Navigation bar bug.

--- Netflix_Clone/Controllers/Core/SearchViewController.swift
+++ Netflix_Clone/Controllers/Core/SearchViewController.swift
@@ -18,14 +18,18 @@ class SearchViewController: UIViewController {
         
     }()
     
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a TV show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
-        
-        
-        
         
         
         view.backgroundColor = .systemBackground
@@ -34,7 +38,13 @@ class SearchViewController: UIViewController {
         discoverTable.delegate = self
         discoverTable.dataSource = self
         
+        navigationItem.searchController = searchController
+        
+        navigationController?.navigationBar.tintColor = .white
+        
         fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
     }
     
     private func fetchDiscoverMovies(){
@@ -81,3 +91,31 @@ extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
 }
+
+extension SearchViewController: UISearchResultsUpdating {
+
+     func updateSearchResults(for searchController: UISearchController) {
+         let searchBar = searchController.searchBar
+
+         guard let query = searchBar.text,
+               !query.trimmingCharacters(in: .whitespaces).isEmpty,
+               query.trimmingCharacters(in: .whitespaces).count >= 3,
+               let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
+                   return
+               }
+
+         APICaller.shared.search(with: query) { result in
+             DispatchQueue.main.async {
+                 switch result {
+                 case .success(let titles):
+                     resultsController.titles = titles
+                     resultsController.searchResultsCollectionView.reloadData()
+                 case .failure(let error):
+                     print(error.localizedDescription)
+                 }
+             }
+         }
+     }
+
+
+ }
